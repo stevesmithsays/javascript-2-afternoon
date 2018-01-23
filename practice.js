c893b353e44d49656f3809474f361851194b73ad
Steve Smith <stevesmithsays@Steves-MacBook-Pro.local>
2018-01-23 14:45:47 -0600

    withoutMethods

--- practice.js
+++ practice.js
@@ -14,7 +14,11 @@
   Then alert your name using dot notation.
 */
 
-//Code here
+var me = {
+  name: 'Steve',
+  age : 29
+};
+alert(me.name);
 
 
 
@@ -25,24 +29,30 @@
   Have the values to those keys be your favorite thing in that category.
 */
 
-//Code here
-
+var favoriteThings = {
+  band: 'glassjaw',
+  food: 'veggies',
+  person: 'Paola',
+  book: 'Linchpin',
+  movie: 'Terminator 2',
+  holiday: 'Christmas'
+};
 
 
 /*
   After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
-
+favoriteThings.car = "toyota";
+favoriteThings.brand = "dlxSF";
 
 
 /*
   Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
-
+favoriteThings.food = 'Chicken Nuggets';
+favoriteThings['book'] = 'Harry Potter';
 
 
 ////////// PROBLEM 3 //////////
@@ -55,16 +65,18 @@
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack. 
 */
 
-//Code here
+var backPack = {
 
-
+};
+var item = "firstPocket";
+backPack[item] = 'chapstick';
+backPack.color = "Blue";
 
 /*
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
-
+alert(backPack);
 
 
 /*
@@ -73,7 +85,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. 
 */
 
-//Code here
+console.log(backPack);
 
 
 
@@ -96,9 +108,8 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
-
-
+user2.name = 'Tyler S. McGinnis';
+user2.email = 'tyler.mcginnis@devmounta.in';
 
 ////////// EXTRA PRACTICE PROBLEMS BELOW //////////
 
@@ -108,8 +119,9 @@ var user2 = {
   Create an empty object called methodCollection.
 */
 
-//Code Here
+var methodCollection = {
 
+};
 
 
 /*
